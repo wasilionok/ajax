d07caf30f2dd25381fc1059d8dcea127353db80b
Agnieszka Wasilionok <agnieszka@wasilionok.pl>
2017-10-29 13:05:11 +0100

    123

---
+++ Ćwiczenie_1/JS/js.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$.getJSON("http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl"), function(data){
+    console.log(data);
+
+function pobierzDane(event){
+
+    console.log("czerwone")
+
+
+    
+}
+$("button").click(pobierzDane);
+    
+}
+
+
+
+
+
+
+
+
